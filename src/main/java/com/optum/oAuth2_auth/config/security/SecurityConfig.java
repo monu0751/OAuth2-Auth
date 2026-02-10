8bb53c1c9342992ed3001adf82c52ef373072c43
Abhinav Sengar <abhinavsengar.cs@gmailcom>
2026-02-10 08:20:33 +0530

    oAuth2 with google for session full login

--- src/main/java/com/optum/oAuth2_auth/config/security/SecurityConfig.java
+++ src/main/java/com/optum/oAuth2_auth/config/security/SecurityConfig.java
@@ -1,47 +1,64 @@
 package com.optum.oAuth2_auth.config.security;
 
 
-import com.optum.oAuth2_auth.config.JwtFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/", "/login").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .oauth2Login(Customizer.withDefaults())
+//                .logout(logout -> logout
+//                        .logoutSuccessUrl("/")
+//                );
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(
+//                                SessionCreationPolicy.STATELESS));
+
+
+//        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/**") // important
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/", "/login", "/ui/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS));
-
-
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .oauth2Login(Customizer.withDefaults())
+                .exceptionHandling(ex -> ex
+                        .defaultAuthenticationEntryPointFor(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                                request -> request.getRequestURI().startsWith("/api/")
+                        )
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                );
 
         return http.build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+
 
 
 
--- src/main/java/com/optum/oAuth2_auth/controllers/HelloController.java
+++ src/main/java/com/optum/oAuth2_auth/controllers/HelloController.java
@@ -1,8 +1,12 @@
 package com.optum.oAuth2_auth.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/hello")
@@ -18,5 +22,12 @@ public class HelloController {
     public String sayGossip() {
         return "Did you hear about the new Java update?";
     }
+
+    @GetMapping("/profile")
+    public Map<String, Object> profile(
+            @AuthenticationPrincipal OAuth2User user) {
+        return user.getAttributes();
+    }
+
 
 }
